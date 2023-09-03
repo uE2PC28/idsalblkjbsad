68721b32c176abe8fdd3c76b545fbde3e6bd85d4
igaolindao <134166860+uE2PC28@users.noreply.github.com>
2023-09-03 14:45:48 -0300

    seo

--- xmlrpc.php
+++ xmlrpc.php
@@ -4,11 +4,11 @@
 		<engineLink>https://wordpress.org/</engineLink>
 		<homePageLink>https://profissaolucrativa.online</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://profissaolucrativa.online/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://profissaolucrativa.online/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://profissaolucrativa.online/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://profissaolucrativa.online/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://profissaolucrativa.online/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://ue2pc28.github.io/idsalblkjbsad/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://ue2pc28.github.io/idsalblkjbsad/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://ue2pc28.github.io/idsalblkjbsad/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://ue2pc28.github.io/idsalblkjbsad/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://ue2pc28.github.io/idsalblkjbsad/wp-json/" />
 			</apis>
 	</service>
 </rsd>
